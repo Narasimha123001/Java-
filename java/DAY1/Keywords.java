67402795578f013d563c309af7a5da7944d29455
Narasimha <narasimhaanguluri001@gmail.com>
2024-06-22 19:54:43 +0530

    file:Literals in Java

---
+++ java/DAY1/Keywords.java
@@ -0,0 +1,19 @@
+package com.Narasimha.java.DAY1;
+
+public class Keywords {
+    public static void main(String[] args) {
+        int age;
+        age = 25;
+
+        String job;
+        job = "Java Developer";
+
+        long pack; //here ,first i gave variable name as "package" its show error "Compile error as we have given keyword as a variable name"
+        //thus why i change its "pack"
+        pack = 50_00_000;
+
+        System.out.println(age+" "+job+" "+pack);
+
+    }
+
+}
---
+++ java/DAY1/Literals.java
@@ -0,0 +1,29 @@
+package com.Narasimha.java.DAY1;
+
+public class Literals {
+    public static void main(String[] args) {
+        int age;
+        age = 25;
+
+        double pi;
+        pi = 3.14159;
+
+        char grade;
+        grade = 'A';
+
+        String message;
+        message = "Hello World!";
+
+        boolean isJavaFun;
+        isJavaFun = true;
+
+        String str;
+        str = null;
+
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(message);
+        System.out.println(isJavaFun);
+        System.out.println(str);
+    }
+}
