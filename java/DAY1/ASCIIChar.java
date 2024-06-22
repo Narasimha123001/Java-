4bc32dc8482b073ea79575b07ccc82b517febf27
Narasimha <narasimhaanguluri001@gmail.com>
2024-06-22 17:21:04 +0530

    Day1:Character Set in java

---
+++ java/DAY1/ASCIIChar.java
@@ -0,0 +1,10 @@
+package com.Narasimha.java;
+
+public class ASCIIChar {
+    public static void main(String[] args) {
+        char letter ='A';
+        int asciiValue = (int) letter;
+        System.out.println("ASCII value of 'A' :" + asciiValue);
+    }
+}
+
---
+++ java/DAY1/UnicodeChar.java
@@ -0,0 +1,12 @@
+package com.Narasimha.java;
+
+
+public class UnicodeChar {
+
+    public static void main(String[] args) {
+        char unicodeChar ='\u0041'; // unicode of A
+        System.out.println("Unicode character: "+ unicodeChar);
+
+    }
+}
+
