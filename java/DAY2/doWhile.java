a9f21a4b968eb784d69a100d41cb84cb3cf3aedf
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-23 01:04:28 +0530

    Do while statment

---
+++ java/DAY2/doWhile.java
@@ -0,0 +1,13 @@
+package com.Narasimha.java.DAY2;
+
+public class doWhile {
+    public static void main(String[] args) {
+        byte no;
+        no = 1;
+        do {
+            System.out.println(no);
+            no++; //here first the code will excetute then its checks the condition
+        }
+        while(no <= 100);
+    }
+}
