7f0ed9d3fac5bd09c947992f2b382c49c9889d0e
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-23 00:46:00 +0530

    some operates in arthmetic

---
+++ java/DAY2/OperatedTest.java
@@ -0,0 +1,13 @@
+package com.Narasimha.java.DAY2;
+
+public class OperatedTest {
+    public static void main(String[] args) {
+        int Salary;
+        Salary =300000;
+
+        int IntrementSalary;
+        IntrementSalary = Salary + (Salary*10) / 100;
+
+        System.out.println("IntrementSalary: "+IntrementSalary);
+    }
+}
