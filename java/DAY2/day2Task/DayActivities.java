b8d492783e70c72d0169d0b4973ec008917774b3
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-23 20:20:53 +0530

    performing SwitchCase Statements

---
+++ java/DAY2/day2Task/DayActivities.java
@@ -0,0 +1,34 @@
+package com.Narasimha.java.DAY2.Day2Task;
+
+public class DayActivities {
+    public static void main(String[] args) {
+        byte day;
+        day = 5;
+        switch(day){
+            case 1:
+                System.out.println("Monday : GYM");
+                break;
+            case 2:
+                System.out.println("Tuesday: YOGA");
+                break;
+            case 3:
+                System.out.println("Wednesday: SWIMMING");
+                break;
+            case 4:
+                System.out.println("Thursday: READING");
+                break;
+            case 5:
+                System.out.println("Friday : MOVIES");
+                break;
+            case 6:
+                System.out.println("Saturday : HIKING");
+                break;
+            case 7:
+                System.out.println("Sunday : REST");
+            default:
+                System.out.println("INVALID");
+        }
+
+    }
+
+}
