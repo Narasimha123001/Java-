02adaa4b7e97425af1ae4aa14fd3d1a3bf246c74
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-23 20:19:18 +0530

    convert the decimal number to binary

---
+++ java/DAY2/day2Task/Convertions.java
@@ -0,0 +1,16 @@
+package com.Narasimha.java.DAY2.Day2Task;
+
+
+public class Convertions {
+    public static void main(String[] args) {
+
+
+        int decimalNumber;
+        decimalNumber =10;
+
+        String binaryNumber = Integer.toBinaryString(decimalNumber);
+        System.out.println("binaryNumber: "+binaryNumber);
+
+    }
+
+}
