3d950459b2eb34e09c27a6e7c62bf104b4bd356e
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-23 20:23:55 +0530

    simple if statement checking For voting system

---
+++ java/DAY2/day2Task/IfStatement.java
@@ -0,0 +1,13 @@
+package com.Narasimha.java.DAY2.Day2Task;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        byte age;
+        age = 58;
+        if(age >= 18){
+            System.out.println("The person is eligible for voting");
+        }else{
+            System.out.println("Not eligible for voting");
+        }
+    }
+}
