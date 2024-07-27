c63cae32c834605f0b69307b0f7b8c36b99778a4
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-28 00:03:00 +0530

    SumOfDigit :Simple program in java

---
+++ java/DAY3/CodingQuestion/SumOfDigits.java
@@ -0,0 +1,17 @@
+package com.Narasimha.java.DAY3.CodingQuestion;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        int sum =0;
+        while(num > 0){
+            sum += num % 10;;
+            num /= 10;
+        }
+        System.out.println("The sum of the digit is: "+sum);
+    }
+}
