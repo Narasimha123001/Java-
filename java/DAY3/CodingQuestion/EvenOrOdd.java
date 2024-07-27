e299cd2f9d565fac4de7561b2c800691eb89c56e
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-28 00:04:08 +0530

    Even or odd

---
+++ java/DAY3/CodingQuestion/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.Narasimha.java.DAY3.CodingQuestion;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0){
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd number");
+        }
+    }
+}
