462bf3d752cfb0b52176f4735371724940c7e463
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-30 00:09:13 +0530

    FloyTriangle pattern

--- java/DAY3/CodingQuestion/Task/FloydTriangle.java
+++ java/DAY3/CodingQuestion/Task/FloydTriangle.java
@@ -2,8 +2,8 @@ package com.Narasimha.java.DAY3.CodingQuestion.Task;
 
 public class FloydTriangle {
     public static void main(String[] args) {
-        int n =5;  //initialize the start row
-        int num =1;   //initialize the starting number
+        int n = 5;  //initialize the start row
+        int num = 1;   //initialize the starting number
         for (int i = 1; i <= n; i++ ) {  // outer loop for row
             for (int j = 1; j <= i; j++) {  // inner loop for inner to print numbers
                 System.out.print(num + " "); // print the current loop
