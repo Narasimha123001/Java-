8381563ee77d2e3df133465efb178ee97785b6f0
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-29 23:53:32 +0530

    PerfectNumber using conditional statements

---
+++ java/DAY2/day2Task/PerfectNumber.java
@@ -0,0 +1,11 @@
+package com.Narasimha.java.DAY2.Day2Task;
+
+public class PerfectNumber {
+}
+/*
+        Exercise 6:
+        Task: Write the code to find if the given number is a perfect number or not?
+        (Perfect number: A perfect number is a positive integer that is equal to the sum of its
+        proper divisors, excluding itself. For example, 6 is a perfect number because its proper
+        divisors are 1, 2, and 3, and their sum is 6.)
+ */
