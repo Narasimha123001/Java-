f1443b6405a483ff9f9ed2109410079b6d580c96
Narasimha <narasimhaanguluri001@gmail.com>
2024-07-30 00:10:29 +0530

    perfect Pyramid pattern

--- java/DAY3/CodingQuestion/Task/Pyramid.java
+++ java/DAY3/CodingQuestion/Task/Pyramid.java
@@ -7,7 +7,7 @@ public class Pyramid {
         for(int i = 1; i<=n; i++){ //row
             for(int j = n-i; j > 0; j--){ // for space and decrement from top to bottom
                 System.out.print("  "); // print the space
-            }for(int j = 1; j<=(2*i-1);j++){// no of star * each inner loop
+            }for(int j = 1; j <= (2*i-1);j++){// no of star * each inner loop
                 System.out.print("* "); // print the stars
             }
             System.out.println();
