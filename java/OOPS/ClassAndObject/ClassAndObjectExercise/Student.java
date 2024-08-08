adfe1b8e77c03a82023774eab394073092a1731e
Narasimha <narasimhaanguluri001@gmail.com>
2024-08-08 21:55:15 +0530

    pratice Program

---
+++ java/OOPS/ClassAndObject/ClassAndObjectExercise/Student.java
@@ -0,0 +1,18 @@
+package com.Narasimha.java.OOPS.ClassAndObject.ClassAndObjectExercise;
+
+public class Student {
+
+    int id=25;
+    String name ;
+    String Gender = "Male";
+
+
+    public static void main(String[] args) {
+
+
+        Student S1 = new Student();
+        System.out.println(S1.id);
+        System.out.println(S1.name);
+        System.out.println(S1.Gender);
+    }
+}
